75c0eabe2c5227090e7284b9382459b9d7d3aa3f
Houston Shane <chapmanh10@gmail.com>
2021-12-10 12:26:14 -0500

    commented JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ var handleLiqourClick = function (liquorchoice) {
     getDrinkID(liquorchoice)
 }
 
+//This function gets a random drink ID from the array called by liquor type
 var getDrinkID = function (liquor) {
     var apiUrl = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + liquor
     fetch(apiUrl)
@@ -16,7 +17,7 @@ var getDrinkID = function (liquor) {
         })
 }
 
-
+// These variables and listeners watch for users to choose a
 var navItemVodka = document.getElementById("vodka")
 var navItemGin = document.getElementById("gin")
 var navItemRum = document.getElementById("rum")
@@ -26,6 +27,7 @@ navItemGin.addEventListener("click", function () { handleLiqourClick("gin") })
 navItemTequila.addEventListener("click", function () { handleLiqourClick("tequila") })
 navItemRum.addEventListener("click", function () { handleLiqourClick("rum") })
 
+//This function takes the random drink ID and gets the information for that drink
 var getCockTail = function (drinkID) {
     var apiUrl = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + drinkID
     fetch(apiUrl)
@@ -39,6 +41,7 @@ var getCockTail = function (drinkID) {
         })
 }
 
+// This function grabs the image from the array and displays it on the page
 var displayImage = function (imageUrl) {
     var image = document.createElement('img')
     image.src = imageUrl
@@ -51,6 +54,7 @@ var displayImage = function (imageUrl) {
     figure.appendChild(image)
 }
 
+// This function creates a list of ingredients
 var displayRecipe = function (data) {
     var mainContainer = document.getElementById("container")
     var drinkName = document.createElement("div")
@@ -67,7 +71,7 @@ var displayRecipe = function (data) {
     var instructionsContainer = document.createElement("div")
     mainContainer.appendChild(instructionsContainer)
 
-
+// This for loop ensures the right amount of ingredients display for the drink
     for (var i = 1; i < 16; i++) {
         var ingredient = data.drinks[0]["strIngredient" + i]
         var amount = data.drinks[0]["strMeasure" + i]
@@ -80,6 +84,7 @@ var displayRecipe = function (data) {
         recipeContainer.append(ingredientText)
     }
 
+// This displays the instructions
     var instructionsText = document.createElement("div")
     instructionsText.textContent = data.drinks[0].strInstructions
     instructionsContainer.appendChild(instructionsText)
