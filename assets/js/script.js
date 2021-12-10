1f06d5d269fa8fa02b6f7e89da619f3ca9d48704
Houston Shane <chapmanh10@gmail.com>
2021-12-10 10:31:35 -0500

    clicking liquor functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,21 @@
 var handleLiqourClick = function (liquorchoice) {
-    getCocktail(liquorchoice)
+    getDrinkID(liquorchoice)
 }
 
-var getCocktail = function(liquor) {
+var getDrinkID = function(liquor) {
     var apiUrl = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + liquor 
     fetch (apiUrl)
         .then(function (response) {
             if (response.ok) {
                 response.json().then(function (data) {
                    var randomNumber = Math.floor(Math.random() * data.drinks.length) 
-                   console.log(randomNumber)
-                    console.log(data.drinks[randomNumber].idDrink)
+                    var drinkID = data.drinks[randomNumber].idDrink
+                    getCockTail(drinkID)
                 })
             }
         })
 }
+
 
 var navItemVodka = document.getElementById("vodka")
 var navItemGin = document.getElementById("gin")
@@ -24,3 +25,23 @@ navItemVodka.addEventListener("click", function(){handleLiqourClick("vodka")})
 navItemGin.addEventListener("click", function(){handleLiqourClick("gin")})
 navItemTequila.addEventListener("click", function(){handleLiqourClick("tequila")})
 navItemRum.addEventListener("click", function(){handleLiqourClick("rum")})
+
+var getCockTail = function(drinkID) {
+    var apiUrl = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + drinkID
+    fetch (apiUrl)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    displayImage(data.drinks[0].strDrinkThumb)
+                })
+            }
+        })
+}
+
+var displayImage = function(imageUrl) {
+    var image = document.createElement('img')
+    image.src = imageUrl
+    var mainContainer = document.getElementById("container")
+    mainContainer.innerHTML = ""
+    mainContainer.appendChild(image)
+}
